ced332464a52fa4211c004cb6e54ba72a247f96a
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-22 13:38:44 +0400

    checked on valid data type before saving in the database

--- src/scripts/saveProduct.js
+++ src/scripts/saveProduct.js
@@ -6,9 +6,32 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const isValidString = (input) => {
+  return /^[A-Za-z\s]+$/.test(input);
+};
+
+const isValidNumber = (input) => {
+  return /^\d+$/.test(input);
+};
+
 rl.question("Enter product name: ", (name) => {
+  if (!isValidString(name)) {
+    console.log("Invalid name. Please enter a valid string.");
+    rl.close();
+    return;
+  }
   rl.question("Enter product price: ", (price) => {
+    if (!isValidNumber(price)) {
+      console.log("Invalid price. Please enter a valid number.");
+      rl.close();
+      return;
+    }
     rl.question("Enter product id: ", async (id) => {
+      if (!isValidNumber(id)) {
+        console.log("Invalid ID. Please enter a valid number.");
+        rl.close();
+        return;
+      }
       await pool.query(
         "INSERT INTO product (name, price, product_id) VALUES ($1, $2, $3)",
         [name, price, id]
