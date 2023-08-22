e432049829c5cad823fa4e606812a497eda11112
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-22 12:49:18 +0400

    project setup

---
+++ src/config/sql.js
@@ -0,0 +1,15 @@
+import pgk from "pg";
+import dotenv from dotenv
+const { Pool } = pgk;
+
+const pool = new Pool({
+    host: process.env.POSTGRE_HOST,
+    port: process.env.POSTGRE_PORT,
+    database: process.env.POSTGRE_DATABASE,
+    user: process.env.POSTGRE_USER,
+    password: process.env.POSTGRE_PASSWORD,
+  });
+
+
+  
+  export default pool;
